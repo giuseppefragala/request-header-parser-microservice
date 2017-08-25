b9fc5701220526a1c604ab5b230e8494bfbe730c
Giuseppe Fragalà <gmfdev2000@hotmail.com>
2017-08-25 11:35:05 +0200

    Adjusting server.js 8.

--- server.js
+++ server.js
@@ -1,39 +1,22 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 var jade = require('jade');
-var bodyParser = require('body-parser')
 
 app.set('views', __dirname + '/');
 app.set('view engine', 'jade');
 
-/*app.get('/', function(req, res) {
-      res.sendFile(path.join(__dirname + '/index.htm'));
-});
-*/
-
 app.get('/', function(req, res) {
 
-	var ip_address = req.connection.remoteAddress;
-	var language = "lang";
-	var software = "soft";
+  
+  var ipaddress = (req.headers['x-forwarded-for'] || '').split(',')[0]  || req.connection.remoteAddress;;
+	var language =  (req.headers["accept-language"]).split(',')[0];
+	var software = (req.headers['user-agent']).split(' ')[1] + " " + (req.headers['user-agent']).split(' ')[2] + (req.headers['user-agent']).split(' ')[3] + " " +(req.headers['user-agent']).split(' ')[4];
+  software = software.replace('(','').replace(')','');
 
-	var output = '{"ip_address": ' + '"' + ip_address + '"' + ', "language": ' + '"' + language + '"' + ', "software": ' + '"' + software + '"}';
-  	res.render('index', { title: 'OUTPUT', head: "OUTPUT", message: output });
+	var output = '{"ipaddress": ' + '"' + ipaddress + '"' + ', "language": ' + '"' + language + '"' + ', "software": ' + '"' + software + '"}';
+  	res.render('index', { title: 'OUTPUT', message: output });
 
-    //res.send(output);
 });
-
-
-// Tell express to use the body-parser middleware and to not parse extended bodies
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// Route that receives a POST request to /
-app.post('/', function (req, res) {
-  const body = req.body
-  res.set('Content-Type', 'text/plain')
-  res.send(`You sent: ${body} to Express`)
-})
 
 
 app.listen(process.env.PORT || 3000)
