19e85604cc77f907756bfdfacb03d05a241a667e
Giuseppe Fragalà <gmfdev2000@hotmail.com>
2017-08-25 11:00:27 +0200

    Adjusting server.js 2.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var moment = require('moment');
 var jade = require('jade');
 var bodyParser = require('body-parser')
 
@@ -14,28 +13,9 @@ app.set('view engine', 'jade');
 */
 
 app.get('/:id', function(req, res) {
-	var input;
-	var unix_output;
-	var natural_output;
-	var date_natural;
-	var date_unix;	
 
-
-	input = req.params.id;
-	date_unix = moment(input, 'X', true).isValid();
-
-	if(moment(input).isValid()){
-		natural_output = input;
-		unix_output = moment(input).unix();
-	}
-	else if(date_unix){
-		natural_output = moment.unix(input).format('YYYY-MMM-DD');;
-		unix_output = input;
-	}
-	else {
-		natural_output = "null";
-		unix_output = "null"
-	};	
+	var unix_output"first";
+	var natural_output "second";
 
 	var output = '{ "unix": ' + unix_output + ', "natural": ' + '"' + natural_output + '" }';
   	res.render('index', { title: 'OUTPUT', head: "OUTPUT", message: output });
